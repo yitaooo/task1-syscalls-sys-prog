3e8f1352b40ac1ea34cd037e91d0d9d2e0fd8098
Chen Yitao <>
2023-07-08 16:15:23 +0200

    Delete

--- librw-1/src/lib.rs
+++
@@ -1,4 +0,0 @@
-#[no_mangle]
-pub extern "C" fn this_function_is_exported_to_c() {
-
-}
--- librw.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-#include <sys/types.h> 
-#include <unistd.h> 
-
-ssize_t read(int __fd, void *__buf, size_t __nbytes);
-ssize_t write(int __fd, const void *__buf, size_t __n);
